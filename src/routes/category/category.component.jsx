7a902931c98b94594990dd447d35475f81587c24
Elvis Cuevas <cuevas.elvis@yahoo.com>
2023-07-03 18:41:26 -0700

    redux thunk to saga

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,13 +1,15 @@
 import './category.styles.scss';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect, Fragment } from 'react';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import { selectCategoriesIsLoading, selectCategoriesMap } from '../../store/categories/category.selector';
 import { useSelector } from 'react-redux';
 import ProductCard from '../../components/product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component';
 
 const Category = () => {
 	const { category } = useParams();
 	const categoriesMap = useSelector(selectCategoriesMap);
+	const isLoading = useSelector(selectCategoriesIsLoading)
 	const [products, setProducts] = useState(categoriesMap[category]);
 
 	useEffect(() => {
@@ -17,12 +19,14 @@ const Category = () => {
 	return (
 		<Fragment>
 			<h2 className='category-title'>{category.toUpperCase()}</h2>
-			<div className='category-container'>
+			{
+				isLoading ? (<Spinner />) : (<div className='category-container'>
 				{ products &&
 					products.map((product) => (
 					<ProductCard key={product.id} product={product} />
 					))}
 			</div>
+			)}
 		</Fragment>
 	)
 };
